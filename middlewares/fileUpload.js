dde09a4e6c64f69da836985ebc6261f3e739bf43
Olosko23 <oloogeorge633@gmail.com>
2023-08-29 18:15:12 +0300

    'file_upload'

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../uploads"); // Destination folder for uploaded files
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
     // Generate a unique filename for the uploaded file
